97d9501041079c4ba4a7054a9253ea4b9ee6d740
fallenrayveil <rahmawanas6@gmail.com>
2023-06-14 22:12:47 +0700

    init

---
+++ ambil_data.php
@@ -0,0 +1,19 @@
+<?php
+include_once('koneksi.php');
+
+// Query untuk mengambil data dari tabel fakta_penjualan
+$query = "SELECT CONCAT(YEAR(tgllengkap), ' ', MONTHNAME(tgllengkap)) AS date, SUM(TotalDue) as totaldue
+FROM fakta_penjualan fp 
+JOIN dimensi_waktu dw ON dw.Date_ID = fp.Date_ID 
+GROUP BY YEAR(tgllengkap), MONTH(tgllengkap);";
+$result = mysqli_query($koneksi, $query);
+
+// Simpan hasil query ke dalam array
+$data = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
+
+// Mengirim data dalam format JSON
+echo json_encode($data);
+?>
